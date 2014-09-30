cd980f925eb4a392b5f59ef99944a7a8896d91d9
unknown <amk66@duke.edu>
2014-09-30 02:43:42 -0400

    added cancel request tests

--- students/tests.py
+++ students/tests.py
@@ -95,6 +95,48 @@ class MultipleStudentsWaitingTestCase(TestCase):
 		self.assertEqual(2, self.student2.get_group_number())
 
 
+class CancelRequestMultipleStudentsWaitingTestCase(TestCase):
+	def setUp(self):
+		user1 = User.objects.create(username='akyker20', 
+								    email='amk66@duke.edu', 
+								    password='hey')
+		user2 = User.objects.create(username='jfuller11', 
+								    email='jfuller@duke.edu', 
+								    password='hey')
+		user3 = User.objects.create(username='tjones04', 
+								    email='tjones@duke.edu', 
+								    password='hey')
+		self.student1 = StudentProfile.objects.create(user=user1)
+		self.student2 = StudentProfile.objects.create(user=user2)
+		self.student3 = StudentProfile.objects.create(user=user3)
+		east = Location.objects.create(name="East Campus")
+		west = Location.objects.create(name="West Campus")
+		central = Location.objects.create(name="Central Campus")
+		self.student1.make_request(east, west)
+		self.student2.make_request(east, central)
+		self.student3.make_request(east, west)
+		self.students = [self.student1, self.student2, self.student3]
+
+	def test_student1_cancels_request(self):
+		self.student1.remove_recent_request()
+		self.assertEqual(False, self.student1.is_waiting_in_group())
+		self.assertEqual(True, self.student2.is_waiting_in_group())
+		self.assertEqual(True, self.student3.is_waiting_in_group())
+		self.assertEqual(None, self.student1.get_group_number())
+		self.assertEqual(2, self.student2.get_group_number())
+		self.assertEqual(1, self.student3.get_group_number())
+
+	def test_student3_cancel_request(self):
+		self.student1.remove_recent_request()
+		self.student3.remove_recent_request()
+		self.assertEqual(False, self.student1.is_waiting_in_group())
+		self.assertEqual(True, self.student2.is_waiting_in_group())
+		self.assertEqual(False, self.student3.is_waiting_in_group())
+		self.assertEqual(None, self.student1.get_group_number())
+		self.assertEqual(1, self.student2.get_group_number())
+		self.assertEqual(None, self.student3.get_group_number())
+
+
 class DriverWithMultipleStudents(TestCase):
 	def setUp(self):
 		user1 = User.objects.create(username='akyker20', 
