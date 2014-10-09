30aef6e7105d97b71445864ac3fdbcd761c2a366
Austin Kyker <amk66@duke.edu>
2014-10-09 18:00:03 -0400

    styling changes adding content holder div

--- kogo/helper.py
+++ kogo/helper.py
@@ -2,6 +2,7 @@ from drivers.models import DriverProfile
 from students.models import StudentProfile
 from django.contrib.auth.models import User
 
+#Determines if the user is a driver
 def is_driver(user):
 	try:
 		user.driverprofile
@@ -9,6 +10,7 @@ def is_driver(user):
 	except:
 		return False
 
+#Determines if the user is a student
 def is_student(user):
 	try:
 		user.studentprofile
