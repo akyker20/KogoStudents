82f4b3a84f94f13d68c952ba45db45b629647750
Austin Kyker <amk66@duke.edu>
2014-11-01 02:47:56 -0400

    final changes to the tests for the night

--- kogo/tests.py
+++ kogo/tests.py
@@ -28,11 +28,11 @@ class FunctionalTests(StaticLiveServerTestCase):
     def get_full_url(self, namespace):
         return "%s%s" % (self.live_server_url, reverse(namespace))
 
-    # def test_admin_site(self):
-    #     # user opens web browser, navigates to admin page
-    #     self.browser.get(self.live_server_url + '/admin/')
-    #     body = self.browser.find_element_by_tag_name('body')
-    #     self.assertIn('Django administration', body.text)
+    def test_admin_site(self):
+        # user opens web browser, navigates to admin page
+        self.browser.get(self.live_server_url + '/admin/')
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Django administration', body.text)
 
     # Helper method to log in the user
     def login(self):
@@ -48,13 +48,13 @@ class FunctionalTests(StaticLiveServerTestCase):
     def select_location(self, loc):
         self.browser.find_element_by_xpath("//form[input/@value='%s']" % loc).submit()
 
-    # def test_login_site(self):
-    #     # user opens web browser, navigates to the student login page
-    #     self.login()
-    #     # Wait until the response is received
-    #     # WebDriverWait(self.browser, 10).until(
-    #     #     lambda driver: self.browser.find_element_by_class_name('pickup-title'))
-    #     self.assertEqual(self.get_full_url('pickup_locations'), self.browser.current_url)
+    def test_login_site(self):
+        # user opens web browser, navigates to the student login page
+        self.login()
+        # Wait until the response is received
+        # WebDriverWait(self.browser, 10).until(
+        #     lambda driver: self.browser.find_element_by_class_name('pickup-title'))
+        self.assertEqual(self.get_full_url('pickup_locations'), self.browser.current_url)
 
     # Simulates the user logging in, and selected West Campus Bus Stop as pickup location
     def test_request_pickup(self):
@@ -72,6 +72,16 @@ class FunctionalTests(StaticLiveServerTestCase):
         self.select_location(pickup_loc)
         self.select_location(dropoff_loc)
         title = self.browser.find_element_by_class_name('request-summary-holder')
-        self.assertTrue(pickup_loc in title.text and dropoff_loc in title.text)
+        self.assertIn(pickup_loc, title.text)
+        self.assertIn(dropoff_loc, title.text)
         group_num = self.browser.find_element_by_class_name('group-number').text
         self.assertEqual(1, int(group_num))
+
+    def test_cancel_request(self):
+        self.login()
+        pickup_loc = 'West Bus Stop'
+        dropoff_loc = 'East Bus Stop'
+        self.select_location(pickup_loc)
+        self.select_location(dropoff_loc)
+        self.browser.find_element_by_class_name('btn').click()
+        self.assertEqual(self.get_full_url('pickup_locations'), self.browser.current_url)
